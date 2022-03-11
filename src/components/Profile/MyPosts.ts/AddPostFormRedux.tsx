2b9bcd7ebeefad61cf3c43fa74d129d57ee8fac6
Natalia <zakrevskaya.97@inbox.ru>
2022-03-11 17:46:07 +0300

    Added styles to AddPostFormRedux.tsx

--- src/components/Profile/MyPosts.ts/AddPostFormRedux.tsx
+++ src/components/Profile/MyPosts.ts/AddPostFormRedux.tsx
@@ -3,6 +3,7 @@ import {InjectedFormProps} from "redux-form/lib/reduxForm";
 import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../Utils/Validators/Validators";
 import {Textarea} from "../../Common/FormsControls/FormsControls";
+import s from "./MyPosts.module.css"
 
 export type AddPostFormType = {
     newPostText: string
@@ -12,13 +13,14 @@ const maxLength10 = maxLengthCreator(10);
 
 export const AddPostForm: React.FC<InjectedFormProps<AddPostFormType>> = (props) => {
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit} className={s.addPostFormBlock}>
             <div>
                 <Field
                     component={Textarea}
                     name={"newPostText"}
                     placeholder={"Write your message"}
                     validate={[required, maxLength10]}
+                    className={s.textField}
                 />
             </div>
             <div>
