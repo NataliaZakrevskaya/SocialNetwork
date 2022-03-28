538bbbc494c9a8b3351f5a77b5702f49c0cb1502
Natalia <zakrevskaya.97@inbox.ru>
2022-03-28 14:50:18 +0300

    Changed auth-reducer.tsx Added InferActionsTypes

--- src/Redux/auth-reducer.tsx
+++ src/Redux/auth-reducer.tsx
@@ -1,28 +1,13 @@
 import {authAPI, securityAPI} from "../api/Api";
 import {Dispatch} from "redux";
 import {stopSubmit} from "redux-form";
-import {AppActionType, AppThunk} from "./redux-store";
+import {AppActionType, AppThunk, InferActionsTypes} from "./redux-store";
 
-
+// CONSTANTS
 const SET_USER_DATA = 'AUTH/SET_USER_DATA';
 const GET_CAPTCHA_URL_SUCCESS = 'AUTH/GET_CAPTCHA_URL_SUCCESS';
 
-type DataType = {
-    id: string,
-    email: string,
-    login: string,
-}
-export type InitialStateType = {
-    data: DataType
-    isAuth: boolean
-    captchaUrl: string | null
-}
-const initialState: InitialStateType = {
-    data: {} as DataType,
-    isAuth: false,
-    captchaUrl: null // if null, then captcha isn't required
-}
-export type AuthReducerActionType = setAuthUserDataType | getCaptchaUrlSuccessType
+
 
 
 const authReducer = (state: InitialStateType = initialState, action: AuthReducerActionType) => {
@@ -46,27 +31,28 @@ const authReducer = (state: InitialStateType = initialState, action: AuthReducer
 }
 
 
-export type setAuthUserDataType = ReturnType<typeof setAuthUserData>
-export const setAuthUserData = (id: string, email: string, login: string, isAuth: boolean) => {
-    return {
-        type: SET_USER_DATA,
-        payload: {id, email, login, isAuth}
-    } as const
+export const authReducerActions = {
+    setAuthUserData: (id: string, email: string, login: string, isAuth: boolean) => {
+        return {
+            type: SET_USER_DATA,
+            payload: {id, email, login, isAuth}
+        } as const
+    },
+    getCaptchaUrlSuccess: (captchaUrl: string) => {
+        return {
+            type: GET_CAPTCHA_URL_SUCCESS,
+            payload: {captchaUrl}
+        } as const
+    }
 }
-export type getCaptchaUrlSuccessType = ReturnType<typeof getCaptchaUrlSuccess>
-export const getCaptchaUrlSuccess = (captchaUrl: string) => {
-    return {
-        type: GET_CAPTCHA_URL_SUCCESS,
-        payload: {captchaUrl}
-    } as const
-}
+
 
 //Thunks
 export const getAuthUserData = (): AppThunk => async (dispatch: Dispatch<AppActionType>) => {
     const response = await authAPI.me()
     if (response.data.resultCode === 0) {
         let {id, email, login} = response.data.data;
-        dispatch(setAuthUserData(id, email, login, true));
+        dispatch(authReducerActions.setAuthUserData(id, email, login, true));
 
     }
 }
@@ -87,14 +73,32 @@ export const login = (email: string, password: string, rememberMe: boolean, capt
 export const getCaptchaUrl = (): AppThunk => async (dispatch: Dispatch<AppActionType>) => {
     const captchaData  = await securityAPI.getCaptchaUrl()
     const captchaUrl = captchaData.url
-    dispatch(getCaptchaUrlSuccess(captchaUrl));
+    dispatch(authReducerActions.getCaptchaUrlSuccess(captchaUrl));
 }
 export const logout = (): AppThunk => async (dispatch: Dispatch<AppActionType>) => {
     let response = await authAPI.logout()
     if (response.data.resultCode === 0) {
-        dispatch(setAuthUserData('', '', '', false));
+        dispatch(authReducerActions.setAuthUserData('', '', '', false));
     }
 
 }
 
+
+//TYPES
+type DataType = {
+    id: string,
+    email: string,
+    login: string,
+}
+export type InitialStateType = {
+    data: DataType
+    isAuth: boolean
+    captchaUrl: string | null
+}
+const initialState: InitialStateType = {
+    data: {} as DataType,
+    isAuth: false,
+    captchaUrl: null // if null, then captcha isn't required
+}
+type AuthReducerActionType = InferActionsTypes<typeof authReducerActions>
 export default authReducer;
--- src/Redux/redux-store.tsx
+++ src/Redux/redux-store.tsx
@@ -1,12 +1,12 @@
-import {applyMiddleware, combineReducers, compose, createStore} from "redux";
-import profileReducer, {ProfileReducerActionType} from "./profile-reducer";
-import dialogsReducer, {DialogsReducerActionType} from "./dialogs-reducer";
+import {Action, applyMiddleware, combineReducers, compose, createStore} from "redux";
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
-import {usersReducer, UsersReducerActionType} from "./users-reducer";
-import authReducer, {AuthReducerActionType} from "./auth-reducer";
+import {usersReducer} from "./users-reducer";
+import authReducer from "./auth-reducer";
 import thunk, {ThunkAction} from "redux-thunk";
 import {reducer as formReducer} from 'redux-form';
-import appReducer, {AppReducerActionType} from "./app-reducer";
+import appReducer from "./app-reducer";
 
 export const rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -19,12 +19,14 @@ export const rootReducer = combineReducers({
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>;
-export type AppActionType =
+
+
+/*export type AppActionType =
     ProfileReducerActionType
     | DialogsReducerActionType
     | UsersReducerActionType
     | AuthReducerActionType
-    | AppReducerActionType
+    | AppReducerActionType*/
 
 //@ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -33,7 +35,13 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 //<то, что возвращает санка (обычно void)>, <типизация стэйта всего приложения>,
 //<unknown>, <типизация экшенов всего приложения>
-export type AppThunk<ReturnType = void> = ThunkAction<void, AppStateType, unknown, AppActionType>
+
+
+/*export type AppThunk<ReturnType = void> = ThunkAction<void, AppStateType, unknown, AppActionType>*/
+export type ThunkType<A extends Action, RT = Promise<void>> = ThunkAction<RT, AppStateType, unknown, A>
+
+type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U: never
+export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesTypes<T>>
 
 export default store;
 
