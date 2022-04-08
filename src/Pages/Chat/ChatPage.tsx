82c66035ff66a9d590104cb1f16b26e96188aaf0
Natalia <zakrevskaya.97@inbox.ru>
2022-04-08 15:43:19 +0300

    Added WS

--- src/Pages/Chat/ChatPage.tsx
+++ src/Pages/Chat/ChatPage.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+const wsChannel = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx");
+
 
 const ChatPage: React.FC = () => {
     return (
@@ -9,6 +12,8 @@ const ChatPage: React.FC = () => {
 }
 
 export const Chat: React.FC = (props) => {
+
+
     return (
         <div>
             <Messages/>
@@ -17,46 +22,61 @@ export const Chat: React.FC = (props) => {
     )
 }
 
+export type ChatMessageType = {
+    message: string
+    photo: string
+    userId: number
+    userName: string
+}
+
+
 export const Messages: React.FC = () => {
 
-    const messages: any = [1, 2, 3, 4]
+    const [messages, setMessages] = useState<ChatMessageType[]>([])
+
+    useEffect(() => {
+        wsChannel.addEventListener('message', (e) => {
+            const newMessages = JSON.parse(e.data)
+            setMessages((prevMessages) => [...prevMessages, ...newMessages])
+        })
+    },[])
+
 
     return (
 
 
         <div style={{height: '400px', overflowY: 'auto'}}>
-            {messages.map((m: any) => <ChatMessage/>)}
-            {messages.map((m: any) => <ChatMessage/>)}
-            {messages.map((m: any) => <ChatMessage/>)}
+            {messages.map((m, index) => <ChatMessage key={index} message={m}/>)}
         </div>
     )
 }
 
-const ChatMessage: React.FC = () => {
-    const message = {
-        url: 'https://cdn1.vectorstock.com/i/1000x1000/51/05/male-profile-avatar-with-brown-hair-vector-12055105.jpg',
-        author: 'Dimych',
-        text: 'Hello, how are you',
-    }
-
+const ChatMessage: React.FC<{message : ChatMessageType}> = ({message}) => {
     return (
         <div>
-            <img src={message.url} width={'30px'} alt={'user image'} /> <b>{message.author}</b>
-            {message.text}
+            <img src={message.photo} width={'30px'} alt={'user image'} /> <b>{message.userName}</b>
+            <br/>
+            {message.message}
             <hr/>
-
         </div>
     )
 }
 
 export const AddChatMessageForm: React.FC = () => {
+    const [message, setMessage] = useState('')
+    const sendMessage = () => {
+        if(!message) return
+        wsChannel.send(message)
+        setMessage('')
+    }
+
     return (
         <div>
             <div>
-                <textarea></textarea>
+                <textarea onChange={(e) => setMessage(e.currentTarget.value)} value={message}></textarea>
             </div>
             <div>
-                <button>Send</button>
+                <button onClick={sendMessage}>Send</button>
             </div>
         </div>
     )
