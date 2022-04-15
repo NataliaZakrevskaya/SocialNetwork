04b4af8b207714988c01e2c8ec0139f0bb40366d
Natalia <zakrevskaya.97@inbox.ru>
2022-04-15 15:40:54 +0300

    Added possibility of moving to user profile from click on img

--- src/Pages/ChatPage/Chat/Messages/ChatMessage.tsx
+++ src/Pages/ChatPage/Chat/Messages/ChatMessage.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import {ChatMessageType} from "../../../../Redux/chat-reducer";
 import s from "./../../ChatPage.module.css";
 import userDefaultPhoto from "./../../../../Images/flat-face-icon-23.png"
+import {NavLink} from "react-router-dom";
 
 export const ChatMessage: React.FC<{ message: ChatMessageType }> = React.memo(({message}) => {
     return (
         <div className={s.message}>
             <div className={s.userInfoContainer}>
-                <img src={message.photo !== null ? message.photo : userDefaultPhoto} alt={'user image'}/>
+                <NavLink to={'/profile/' + message.userId}>
+                <img
+                    src={message.photo !== null ? message.photo : userDefaultPhoto}
+                    alt={'user image'}
+                />
+                </NavLink>
                 <span>{message.userName}</span>
+
             </div>
             <span className={s.messageText}>{message.message}</span>
         </div>
