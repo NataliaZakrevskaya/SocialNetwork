71e4b1a905c28eb1aa0cf79c76210bc2189b475f
Natalia <zakrevskaya.97@inbox.ru>
2022-04-16 13:49:59 +0300

    Fixed auth redirect

--- src/components/Users/UsersPage.tsx
+++ src/components/Users/UsersPage.tsx
@@ -3,13 +3,19 @@ import {useSelector} from 'react-redux';
 import {Users} from './Users';
 import {Preloader} from "../Common/Preloader/Preloader";
 import {getIsFetching} from "../../Redux/Selectors/users-selectors";
+import {AppStateType} from "../../Redux/redux-store";
+import {Navigate} from "react-router-dom";
 
 
 const UsersPage: React.FC<UsersPropsType> = (props) => {
 
 const isFetching = useSelector(getIsFetching)
+    const isAuth = useSelector<AppStateType, boolean>(state => state.auth.isAuth)
 
-        return (
+    if (!isAuth) {
+        return <Navigate to='/login'/>
+    }
+    return (
             <>
                 {isFetching ? <Preloader/> : null}
                 <Users/>
