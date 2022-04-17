4f9adfbf41e8db565c79a2c66d4deee9a99ba8eb
Natalia <zakrevskaya.97@inbox.ru>
2022-04-17 20:28:29 +0300

    Fixed adopting profile page (before max-width: 800px)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import './App.css'
 import Navbar from "./components/Navbar/Navbar";
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import {HashRouter, Navigate, Route, Routes} from 'react-router-dom';
 import HeaderContainer from "./components/Header/HeaderContainer";
 import {connect, Provider} from "react-redux";
 import {initializeApp} from "./Redux/app-reducer";
 import store, {AppStateType} from "./Redux/redux-store";
 import {Preloader} from "./components/Common/Preloader/Preloader";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+import Loading from "./components/Common/Loader/Loading";
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 const UsersPage = React.lazy(() => import('./components/Users/UsersPage'));
 const LoginPage = React.lazy(() => import('./components/Login/LoginPage'));
 const ChatPage = React.lazy(() => import('./Pages/ChatPage/ChatPage'));
 
-type MapStateToPropsType = {
-    initialized: boolean
-}
-type MapDispatchToPropsType = {
-    initializeApp: () => void
-}
-type AppPropsType = MapStateToPropsType & MapDispatchToPropsType
 
 
 class App extends React.Component<AppPropsType> {
@@ -54,11 +48,11 @@ class App extends React.Component<AppPropsType> {
                         <Route path='/profile' element={<ProfileContainer/>}>
                             <Route path=":userId" element={<ProfileContainer/>}/>
                         </Route>
+
                         <Route path='/users' element={<UsersPage/>}/>
                         <Route path='/login' element={<LoginPage/>}/>
                         <Route path='/chat' element={<ChatPage/>}/>
                         <Route path='*' element={<div>404 NOT FOUND</div>}/>
-
                     </Routes>
                 </div>
             </main>
@@ -70,16 +64,26 @@ const mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
     initialized: state.app.initialized
 });
 
-const AppContainer = connect(mapStateToProps, {initializeApp})(App);
+const AppContainer = connect<MapStateToPropsType, MapDispatchToPropsType, {}, AppStateType>(mapStateToProps, {initializeApp})(App);
 
 export const MainApp = () => {
     return (
-        <React.Suspense fallback={<div>Loading...</div>}>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <React.Suspense fallback={<Loading/>}>
+            <HashRouter>
                 <Provider store={store}>
                     <AppContainer/>
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </React.Suspense>
     )
 }
+
+// TYPES
+
+type MapStateToPropsType = {
+    initialized: boolean
+}
+type MapDispatchToPropsType = {
+    initializeApp: () => void
+}
+type AppPropsType = MapStateToPropsType & MapDispatchToPropsType
